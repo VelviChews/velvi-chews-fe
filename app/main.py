59c6ef227b8c06d4550a7448277611c0c157a193
dasihayu <dasihayu1307@gmail.com>
2026-03-06 10:27:41 +0700

    feat: Initialize FastAPI application with database setup, CORS, static file serving, and API router registration.

--- app/main.py
+++ app/main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .database import Base, engine, SessionLocal
 from .models import user, ar_card, scan_history, redeem_item, redeem_history
-from app.routers import auth_route, redeem_item_route, user_route, redeem_history_route, ar_card_route, scan_route
+from app.routers import auth_route, redeem_item_route, user_route, redeem_history_route, scan_route
+from app.routers.ar_card_route import router as ar_card_router
 from fastapi.staticfiles import StaticFiles
 import os
 
@@ -36,7 +37,7 @@ app.include_router(auth_route.router)
 app.include_router(user_route.router)
 app.include_router(redeem_item_route.router)
 app.include_router(redeem_history_route.router)
-app.include_router(ar_card_route.router)
+app.include_router(ar_card_router)
 app.include_router(scan_route.router)
 
 # Seeder otomatis saat startup
